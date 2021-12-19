b32b2f9f258e1882246dc90f0ee9ffc9360a2c65
Pace <pacex3123@gmail.com>
2021-12-19 21:18:19 +0100

    Helikopter meme

---
+++ src/Helikopter.java
@@ -0,0 +1,49 @@
+public class Helikopter {
+
+    public static void main(String[] args) throws InterruptedException {
+        while (true) {
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+
+            System.out.println(" Helikopter Helikopter");
+            System.out.println("           A           ");
+            System.out.println("       /--------       ");
+            System.out.println(" LOL====      ()\\     ");
+            System.out.println("  L    \\         \\        ");
+            System.out.println("        \\_________]                ");
+            System.out.println("           I     I      ");
+            System.out.println("        -----------/              ");
+            System.out.println("                       ");
+            System.out.println("      Helikopter Helikopter              ");
+
+            Thread.sleep(100);
+
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+
+            System.out.println(" Helikopter Helikopter");
+            System.out.println("           A           ");
+            System.out.println("       /--------       ");
+            System.out.println(" LOL====      ()\\     ");
+            System.out.println("  L    \\         \\        ");
+            System.out.println("        \\_________]                ");
+            System.out.println("           I     I      ");
+            System.out.println("        -----------/              ");
+            System.out.println("                       ");
+            System.out.println("      Helikopter Helikopter              ");
+            Thread.sleep(100);
+        }
+    }
+}
